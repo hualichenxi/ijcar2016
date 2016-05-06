00d5263c9c54e694df80ffd8bc647d681eabe00c
ChenJiaoyan <chen00217@gmail.com>
2016-05-06 22:13:59 +0800

    feature example

--- sql/features/um_f.sql
+++ sql/features/um_f.sql
@@ -7,7 +7,7 @@ tr_d left join (
 select User_id,Merchant_id,1 as Um_buy from ijcai2016_koubei_train where Time_Stamp<="2015-09-30 00:00:00"
 ) as f
 on tr_d.User_id=f.User_id and tr_d.Merchant_id = f.Merchant_id
-group by (tr_d.User_id,tr_d.Merchant_id)
+group by tr_d.User_id,tr_d.Merchant_id
 )
 ;
 create table Um_buy as(
