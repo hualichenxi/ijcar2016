84e50dcbd4b2c3738947401e49590274af277cb2
hualichenxi <hualichenxi@163.com>
2016-05-15 01:00:24 -0400

    add feature sql and python for click_category

--- sql/用户_商家/用户买过商家的次数_3days.sql
+++ sql/用户_商家/用户买过商家的次数_3days.sql
@@ -1,39 +1,51 @@
 -- -----------------------------train----------------------------------------
 -- generate features:
 
-create table if not exists tmp_xx as(
-select User_id, Merchant_id,count(*) as count from train_before_10 
-group by User_id,Merchant_id
-where datediff("2015-10-01 00:00:00",Time_Stamp)>=1 and  datediff("2015-10-01 00:00:00",Time_Stamp)<=3
+create table tmp_x as (
+select * from train_before_10
+where  datediff("2015-10-01 00:00:00",Time_Stamp)>=1 and  datediff("2015-10-01 00:00:00",Time_Stamp)<=3
 );
 
+create table if not exists tmp_xx as(
+select User_id, Merchant_id,count(*) as count from tmp_x 
+group by User_id,Merchant_id
+);
+
+create index idx_index on tmp_xx(User_id,Merchant_id);
+	
 create table train_feature_buy_merchant_3days(
 select A.User_id,A.Location_id,A.Merchant_id,IFNULL(B.count,0) 
 as User_Merchant_count from tr_d_new as A left join tmp_xx as B on 
 A.User_id=B.User_id and A.Merchant_id=B.Merchant_id
 );
 
-drop tmp_xx;
-
-create index idx_feature_train_buy_merchant_3days on train_feature_buy_merchant_3days(User_id,Location_id,Merchant_id);
+drop table tmp_xx;
+drop table tmp_x;
+create index idx_feature_train1_buy_merchant_3days on train_feature_buy_merchant_3days(User_id,Location_id,Merchant_id);
 
 
 
 -- -----------------------------test----------------------------------------
-
-create table if not exists tmp_xx as(
-select User_id, Merchant_id,count(*) as count from ijcai2016_koubei_train 
-group by User_id,Merchant_id
-where datediff("2015-12-01 00:00:00",Time_Stamp)>=1 and  datediff("2015-12-01 00:00:00",Time_Stamp)<=3
+create table tmp_x as (
+select * from ijcai2016_koubei_train
+where  datediff("2015-12-01 00:00:00",Time_Stamp)>=1 and  datediff("2015-12-01 00:00:00",Time_Stamp)<=3
 );
 
-create table train_feature_buy_merchant_3days(
+create table if not exists tmp_xx as(
+select User_id, Merchant_id,count(*) as count from  tmp_x
+group by User_id,Merchant_id
+);
+create index idx_index on tmp_xx(User_id,Merchant_id);
+
+
+
+create table test_feature_buy_merchant_3days(
 select A.User_id,A.Location_id,A.Merchant_id,IFNULL(B.count,0) 
 as User_Merchant_count from te_d_new as A left join tmp_xx as B on 
 A.User_id=B.User_id and A.Merchant_id=B.Merchant_id
 );
 
-drop tmp_xx;
-
+drop  table tmp_xx;
+drop  table tmp_x;
 create index idx_feature_test_buy_merchant_3days on test_feature_buy_merchant_3days(User_id,Location_id,Merchant_id);
 
